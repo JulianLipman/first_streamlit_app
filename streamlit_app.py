00a51c8550ebd924b864426cbc82a86deb07a093
Julian Lipman <116745509+JulianLipman@users.noreply.github.com>
2022-11-28 10:28:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Mum'''s New Healthy Diner')
+streamlit.title('My Mums New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
